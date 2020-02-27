dfbdb9698cc4adb3ef10748b1d53b7400bb0f198
Nadezhda Kazynkina <nadezhda.kaa@gmail.com>
2020-02-27 10:54:12 +0300

    Gson exercise was finished

--- app/src/main/java/com/example/photogallery/FlickrFetchr.java
+++ app/src/main/java/com/example/photogallery/FlickrFetchr.java
@@ -3,6 +3,9 @@ package com.example.photogallery;
 import android.net.Uri;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,6 +14,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -71,24 +75,53 @@ public class FlickrFetchr {
         return items;
     }
 
+//    private void parseItmes(List<GalleryItem> items, JSONObject jsonBody) throws IOException, JSONException{
+//        JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
+//        JSONArray photosJsonArray = photosJsonObject.getJSONArray("photo");
+//
+//        for (int i =0; i < photosJsonArray.length(); i++){
+//            JSONObject photoJsonObject = photosJsonArray.getJSONObject(i);
+//
+//            GalleryItem item = new GalleryItem();
+//            item.setId(photoJsonObject.getString("id"));
+//            item.setCaption(photoJsonObject.getString("title"));
+//
+//            if (!photoJsonObject.has("url_s")) {
+//                Log.i(TAG, "has not");
+//                continue;
+//            }
+////            Log.i(TAG, "size" + items.size());
+//            item.setUrl(photoJsonObject.getString("url_s"));
+//            items.add(item);
+//        }
+//    }
+
     private void parseItmes(List<GalleryItem> items, JSONObject jsonBody) throws IOException, JSONException{
+        Gson gson = new Gson();
+        Type galleryItemType = new TypeToken<ArrayList<GalleryItem>>(){}.getType();
         JSONObject photosJsonObject = jsonBody.getJSONObject("photos");
         JSONArray photosJsonArray = photosJsonObject.getJSONArray("photo");
 
-        for (int i =0; i < photosJsonArray.length(); i++){
-            JSONObject photoJsonObject = photosJsonArray.getJSONObject(i);
+        String jsonPhotosString = photosJsonArray.toString();
 
-            GalleryItem item = new GalleryItem();
-            item.setId(photoJsonObject.getString("id"));
-            item.setCaption(photoJsonObject.getString("title"));
+        List<GalleryItem> galleryItemList = gson.fromJson(jsonPhotosString,galleryItemType);
+        items.addAll(galleryItemList);
 
-            if (!photoJsonObject.has("url_s")) {
+
+//        for (int i =0; i < photosJsonArray.length(); i++){
+//            JSONObject photoJsonObject = photosJsonArray.getJSONObject(i);
+//
+//            GalleryItem item = new GalleryItem();
+//            item.setId(photoJsonObject.getString("id"));
+//            item.setCaption(photoJsonObject.getString("title"));
+//
+//            if (!photoJsonObject.has("url_s")) {
 //                Log.i(TAG, "has not");
-                continue;
-            }
-//            Log.i(TAG, "size" + items.size());
-            item.setUrl(photoJsonObject.getString("url_s"));
-            items.add(item);
-        }
+//                continue;
+//            }
+//            //Log.i(TAG, "size" + items.size());
+//            item.setUrl(photoJsonObject.getString("url_s"));
+//            items.add(item);
+//        }
     }
 }
--- app/src/main/java/com/example/photogallery/GalleryItem.java
+++ app/src/main/java/com/example/photogallery/GalleryItem.java
@@ -2,9 +2,33 @@ package com.example.photogallery;
 
 import androidx.annotation.NonNull;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class GalleryItem {
+    @SerializedName("title")
+    @Expose()
     private String mCaption;
+    @SerializedName("id")
+    @Expose()
     private String mId;
+    @SerializedName("url_s")
+    @Expose()
+    @JsonRequired
     private String mUrl;
 
 
@@ -37,4 +61,44 @@ public class GalleryItem {
         mUrl = url;
     }
 
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
+    @interface JsonRequired
+    {
+    }
+
+    class AnnotatedDeserializer<T> implements JsonDeserializer<T>
+    {
+
+        public T deserialize(JsonElement je, Type type, JsonDeserializationContext jdc) throws JsonParseException
+        {
+            T pojo = new Gson().fromJson(je, type);
+
+            Field[] fields = pojo.getClass().getDeclaredFields();
+            for (Field f : fields)
+            {
+                if (f.getAnnotation(JsonRequired.class) != null)
+                {
+                    try
+                    {
+                        f.setAccessible(true);
+                        if (f.get(pojo) == null)
+                        {
+                            throw new JsonParseException("Missing field in JSON: " + f.getName());
+                        }
+                    }
+                    catch (IllegalArgumentException ex)
+                    {
+                        Logger.getLogger(AnnotatedDeserializer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    catch (IllegalAccessException ex)
+                    {
+                        Logger.getLogger(AnnotatedDeserializer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+            return pojo;
+
+        }
+    }
 }
